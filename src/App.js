113665bfb771381ff50c65436b9e1f1e45252617
ttran9 <toddtran9@gmail.com>
2021-01-26 22:01:52 -0800

    creating function based app to demonstrate basic rendering.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import "./index.css";
 
-class App extends Component {
-    render() {
-        return (
-              <div>This is some filler content!</div>  
-        )
+export default function App() {
 
-    }
+    const username = null; 
+    // const username = 'miguel';
+    const todos = [
+        'buy groceries',
+        'buy newspaper'
+    ]
+    // above is more "logic rather than presentation."
 
+    // below just generates presentation.
+    return (
+        <div className="App">
+            <h1>Part 1: Rendering</h1>
+            {username ?
+            <h2>Hello, {username} </h2>
+            :
+            <h2>Hello, Stranger</h2>
+            }
+            {username &&
+                <p>Logout</p>
+            }
+
+            <ul>
+                {todos.map(todo => <li>{todo}</li>)}
+            </ul>
+        </div>
+    )
 }
-
-export default App;
